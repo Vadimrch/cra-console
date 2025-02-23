78dded305dedb6bf523d4f62529d2e16f235451f
KorolenkoVadim <korolenkovadim@gmail.com>
2025-02-23 20:57:56 +0300

    Add idea ignor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Menu>
+<ul>
+    <li><a href="">menu item 1</a></li>
+    <li><a href="">menu item 2</a></li>
+    <li><a href="">menu item 3</a></li>
+</ul>
+        </Menu>
+
+        <Box>
+      <StyledBtn as={Link} href={"#"}>LinkComponent</StyledBtn>
+      <StyledBtn as="a" href={"#"}>Link</StyledBtn>
+      <StyledBtn>Hello</StyledBtn>
+            <SuperButton>Super Button</SuperButton>
+        </Box>
     </div>
   );
 }
 
 export default App;
+
+const StyledBtn = styled.button`
+    border: none;
+    background-color: #fb3f78;
+    padding: 10px 20px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: bold;
+
+    &:hover {
+        background-color: #684abf;
+    }
+
+    &:last-child {
+        background-color: #b2f147;
+    }
+`
+const Link = styled.a`
+    color: #297bf6;
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: transparent;
+    padding: 0;
+`
+const SuperButton = styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: gold;
+    color: #686666;
+`
+
+const Box = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    
+    button{
+        cursor: pointer;
+    }
+    
+    ${Link} {
+        cursor: zoom-in;
+    }
+`
+const Menu = styled.nav`
+ul{
+    list-style: none;
+    padding: 0;
+    display: flex;
+    
+    li > a{
+        color: green;
+    }
+    
+    li + li {
+        margin-left: 20px;
+    }
+}
+    
+`
